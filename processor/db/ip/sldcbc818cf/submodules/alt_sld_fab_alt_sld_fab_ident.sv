75861612d4b74741d46c8eec42b1a250cfad82fb
KENTO UEDA <ueda.kento.42z@st.kyoto-u.ac.jp>
2022-05-20 16:29:09 +0900

    Ueda's 0520 Commit. finish Sort Contest

--- processor/db/ip/sldcbc818cf/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ processor/db/ip/sldcbc818cf/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : dc594a59fd7ebba9fb1b
+//   DESIGN_HASH   : ec9757cc1d6f006e44e1
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hdc594a59fd7ebba9fb1b };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hec9757cc1d6f006e44e1 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
---
+++ processor/fastclock_inst.v
@@ -0,0 +1,6 @@
+fastclock	fastclock_inst (
+	.areset ( areset_sig ),
+	.inclk0 ( inclk0_sig ),
+	.c0 ( c0_sig ),
+	.locked ( locked_sig )
+	);
